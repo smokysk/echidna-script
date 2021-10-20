6e4b7b68cf00567fce46b0c0aa5005113787c938
samuel aka smokysk <samuelviana2626@mail.com>
2021-10-20 18:45:14 -0400

    first commit

---
+++ ict.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+sudo apt install git build-essential gcc automake autoconf make 
+
+git clone https://github.com/enki/libev.git
+
+git clone https://github.com/hyperrealm/libconfig.git 
+
+git clone https://github.com/protocolbuffers/protobuf.git 
+
+git clone https://github.com/protobuf-c/protobuf-c.git 
+
+git clone https://github.com/nemequ/munit.git 
+
+git clone https://github.com/gcovr/gcovr.git
